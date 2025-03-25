18403d4a876fd227565cb40d92035d03c30b3bd7
Faez mehrabani <f.mehrabani1986@gmail.com>
2025-03-24 23:20:15 -0400

    fix responsive issue for table and search input

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -21,50 +21,56 @@ const Table: FC<TableProps> = ({
   isFavorits = false,
 }) => {
   return (
-    <table className={classes.table}>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>State/Province</th>
-          <th>Website</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id}>
-            <td>{item.name}</td>
-            <td>{item.state}</td>
-            <td>
-              <a href={item.website} target="_blank" rel="noopener noreferrer">
-                {item.website}
-              </a>
-            </td>
-            <td>
-              {isFavorits ? (
-                <button
-                  onClick={() => onToggleFavorite(item.university_id)}
-                  className={classes.isFavorite}
-                >
-                  Remove from Favorites
-                </button>
-              ) : (
-                <button
-                  onClick={() => onToggleFavorite(item.id)}
-                  className={
-                    favorites?.has(item.id) ? classes.isFavorite : undefined
-                  }
-                >
-                  {favorites?.has(item.id)
-                    ? "Remove from Favorites"
-                    : "Add to Favorites"}
-                </button>
-              )}
-            </td>
+    <div className={classes["table-container"]}>
+      <table className={classes.table}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>State/Province</th>
+            <th>Website</th>
+            <th>Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.state}</td>
+              <td>
+                <a
+                  href={item.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.website}
+                </a>
+              </td>
+              <td>
+                {isFavorits ? (
+                  <button
+                    onClick={() => onToggleFavorite(item.university_id)}
+                    className={classes.isFavorite}
+                  >
+                    Remove from Favorites
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => onToggleFavorite(item.id)}
+                    className={
+                      favorites?.has(item.id) ? classes.isFavorite : undefined
+                    }
+                  >
+                    {favorites?.has(item.id)
+                      ? "Remove from Favorites"
+                      : "Add to Favorites"}
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
